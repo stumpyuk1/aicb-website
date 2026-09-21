3e4a5eb55e37519bd0695ab88c9b2a0db6618dc0
Adrian Shaw <stumpyuk1@gmail.com>
2026-09-21 11:46:08 +0100

    Embed the street-and-viaduct banner as a webp data URI for the home hero

--- lib/banner-uri.ts
+++ lib/banner-uri.ts
@@ -1,5 +1,2 @@
-import { a } from "./banner-a";
-import { b } from "./banner-b";
-import { c } from "./banner-c";
-
-export const bannerSrc = "data:image/webp;base64," + a + b + c;
+export const bannerSrc =
+  "PLACEHOLDER_WILL_REPLACE";
