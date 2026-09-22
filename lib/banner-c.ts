3cf528539dd21ab17d1f6faee1304927372ce7c7
Adrian Shaw <stumpyuk1@gmail.com>
2026-09-22 15:31:10 +0100

    Restore photographic hero banner (chunk b + assembler)

--- lib/banner-c.ts
+++ lib/banner-c.ts
@@ -1,2 +1,1 @@
-export const c =
-  "PLACEHOLDER_C";
+export const c = "";
--- lib/banner-uri.ts
+++ lib/banner-uri.ts
@@ -1,6 +1,4 @@
 import { a } from "./banner-a";
 import { b } from "./banner-b";
-import { c } from "./banner-c";
-import { d } from "./banner-d";
 
-export const bannerSrc = `data:image/jpeg;base64,${a}${b}${c}${d}`;
+export const bannerSrc = `data:image/jpeg;base64,${a}${b}`;
